8df1d68511e1c01184134bd8029bd00fa7774f92
Lucas <potronlucas9@outlook.fr>
2022-09-15 18:28:53 +0200

    Add Table: Project and Task for SQLite room and add key for table

--- app/src/main/java/com/cleanup/todoc/model/Task.java
+++ app/src/main/java/com/cleanup/todoc/model/Task.java
@@ -3,6 +3,9 @@ package com.cleanup.todoc.model;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
 
 import java.util.Comparator;
 
@@ -11,10 +14,21 @@ import java.util.Comparator;
  *
  * @author Gaëtan HERFRAY
  */
+
+@Entity(foreignKeys = @ForeignKey(entity = Project.class,  // table Task
+
+    parentColumns = "id",
+
+    childColumns = "projectId"))
+
+
 public class Task {
     /**
      * The unique identifier of the task
      */
+
+    @PrimaryKey(autoGenerate = true) // Room generate unique id automatically
+
     private long id;
 
     /**
